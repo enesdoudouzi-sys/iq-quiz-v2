16f8a98aa864f89f4167005b03c943890c0dc751
enes <enes@enes-Lenovo-V15-G2-ALC>
2026-05-30 09:12:26 +0200

    Service Worker hinzugefuegt

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = 'iq-quiz-v1';
+const FILES = [
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){
+      return c.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request);
+    })
+  );
+});
